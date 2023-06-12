09afd2994792e7deafe8c3f35f4c3c68f7c08269
Luca Van Genechten <r0849476@student.thomasmore.be>
2023-06-12 14:26:13 +0200

    added multilingual pdf

--- components/Resume.js
+++ components/Resume.js
@@ -1,14 +1,20 @@
 import { useTranslations } from "next-intl";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 
 export default function Resume() {
   const t = useTranslations("Resume");
+  const router = useRouter();
+  const href =
+    router.locale == "en"
+      ? "/cv_luca_van_genechten_en.pdf"
+      : "/cv_luca_van_genechten_nl.pdf";
   return (
     <div className="flex flex-row justify-center items-start overflow-hidden mt-5">
       <div className="w-full md:w-1/2 max-w-md mx-auto text-center md:text-left lg:p-20">
         <Link
-          href="/cv_luca_van_genechten.pdf"
+          href={href}
           className="mb-20 md:mb-0 px-8 py-4 rounded-md bg-white shadow-lg text-xl font-semibold flex flex-row space-x-4 justify-evenly items-center dark:text-gray-700"
           target="_blank"
           rel="noopener noreferrer"
