e1dc72df1017be4125dede26d7446195f7380b54
Luca Van Genechten <r0849476@student.thomasmore.be>
2023-06-12 13:58:53 +0200

    resume updated, navbar updated

--- components/Resume.js
+++ components/Resume.js
@@ -11,7 +11,6 @@ export default function Resume() {
           href="/cv_luca_van_genechten.pdf"
           className="mb-20 md:mb-0 px-8 py-4 rounded-md bg-white shadow-lg text-xl font-semibold flex flex-row space-x-4 justify-evenly items-center dark:text-gray-700"
           target="_blank"
-          locale="false"
           rel="noopener noreferrer"
         >
           <svg
