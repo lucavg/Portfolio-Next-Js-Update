2924264fee58f266f1c6af77015c8acfcf3da4ef
Luca Van Genechten <r0849476@student.thomasmore.be>
2023-06-13 09:24:04 +0200

    about me resume link updated

--- components/Resume.js
+++ components/Resume.js
@@ -6,15 +6,15 @@ import React from "react";
 export default function Resume() {
   const t = useTranslations("Resume");
   const router = useRouter();
-  const href =
+  const resumeLink =
     router.locale == "en"
       ? "/cv_luca_van_genechten_en.pdf"
-      : "/cv_luca_van_genechten_nl.pdf";
+      : "/nl/cv_luca_van_genechten_nl.pdf";
   return (
     <div className="flex flex-row justify-center items-start overflow-hidden mt-5">
       <div className="w-full md:w-1/2 max-w-md mx-auto text-center md:text-left lg:p-20">
         <Link
-          href={href}
+          href={resumeLink}
           className="mb-20 md:mb-0 px-8 py-4 rounded-md bg-white shadow-lg text-xl font-semibold flex flex-row space-x-4 justify-evenly items-center dark:text-gray-700"
           target="_blank"
           rel="noopener noreferrer"
